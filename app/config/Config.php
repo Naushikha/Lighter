973231aa80b1dfe6b33b9115af6e643c911245e1
Naushikha <naushikha.jayawickrama@gmail.com>
2021-04-13 11:56:33 +0530

    Changed folder name from lighter to lighter-framework

--- app/config/Config.php
+++ app/config/Config.php
@@ -4,7 +4,7 @@
 
 define('APP_TITLE', 'Lighter Framework'); // Shown at the browser title bar
 
-define('BASEURL', 'http://localhost/lighter'); //Define without trailing '/'
+define('BASEURL', 'http://localhost/lighter-framework'); //Define without trailing '/'
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'lighter');
